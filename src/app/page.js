2bdde0116b28d07ef0a3445ebb6e2826ea105905
abdullah-25 <imv.abdullah@mail.utoronto.ca>
2023-09-14 01:03:46 -0400

    layout of homepage with data coming from an API

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,79 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import Image from "next/image";
+import styles from "./page.module.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import sunny from "../../public/sunny.svg";
 
 export default function Home() {
+  const [temp, setTemp] = useState("");
+  const [FiveDayTemp, setFiveDayTemp] = useState(Array(5).fill(null));
+
+  function FiveDayHistoricalData(data) {
+    // Create an array to store the 5 values
+    const tempArray = [];
+
+    for (let i = 0; i < 5; i++) {
+      // Calculate the index based on your logic
+      const index = i * 24;
+      if (data[index]) {
+        tempArray.push(data[index]);
+      }
+    }
+
+    // Set the entire array in one go
+    setFiveDayTemp(tempArray);
+  }
+  console.log({ sunny });
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.open-meteo.com/v1/forecast?latitude=40.71&longitude=-74.01&hourly=temperature_2m&start_date=2023-05-31&end_date=2023-06-04&timezone=America%2FNew_York"
+      )
+      .then((res) => {
+        console.log(res.data);
+        setTemp(res.data.hourly.temperature_2m[119]);
+        FiveDayHistoricalData(res.data.hourly.temperature_2m);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <div className="p-6 max-w-sm bg-white rounded-xl shadow-md flex flex-col items-center space-y-4">
+        <div className="flex-shrink-0">
+          <Image src={sunny} alt="Sunny" width={100} height={100} />
         </div>
+        <div className="flex space-x-2">
+          {" "}
+          <div className="text-xl font-medium text-black">
+            Current Temperature:
+          </div>
+          <div className="text-xl font-medium text-black">{temp}</div>
+        </div>
+
+        <div className="flex flex-col items-center ">
+          <div className="flex space-x-4">
+            {FiveDayTemp.map((temp, i) => (
+              <div className="text-gray-500" id={i} key={i}>
+                {temp}
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+          Save
+        </button>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
